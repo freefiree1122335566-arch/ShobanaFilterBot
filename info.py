e9d215902376b6e22f4692933ef113684d6580b8
freefiree1122335566-arch <freefiree1122335566@gmail.com>
2026-01-03 11:39:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -41,7 +41,7 @@ DEFAULT_AUTH_CHANNELS = [int(x) for x in environ.get("AUTH_CHANNEL", "1003320621
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://messiuuumessiuuu31_db_user:Adarsh12345@cluster0.vs5ille.mongodb.net/?appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'mn_files')
 
